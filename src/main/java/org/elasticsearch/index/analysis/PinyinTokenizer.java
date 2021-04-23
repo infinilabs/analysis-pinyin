e102634b96b4c814f2f20942db042ad2b1acf596
Kun Zhu <kun.zhu@moodys.com>
2021-04-23 11:03:26 +0800

    BSP-135: fix search Chinese character with same pinyin issue.
    Refer to https://www.cnblogs.com/danvid/p/10691547.html
    https://github.com/medcl/elasticsearch-analysis-pinyin/issues/203

--- src/main/java/org/elasticsearch/index/analysis/PinyinTokenizer.java
+++ src/main/java/org/elasticsearch/index/analysis/PinyinTokenizer.java
@@ -45,7 +45,7 @@ public class PinyinTokenizer extends Tokenizer {
         this.config = config;
 
         //validate config
-        if (!(config.keepFirstLetter || config.keepSeparateFirstLetter || config.keepFullPinyin || config.keepJoinedFullPinyin)) {
+        if (!(config.keepFirstLetter || config.keepSeparateFirstLetter || config.keepFullPinyin || config.keepJoinedFullPinyin || config.keepSeparateChinese)) {
             throw new ConfigErrorException("pinyin config error, can't disable separate_first_letter, first_letter and full_pinyin at the same time.");
         }
         candidate = new ArrayList<>();
@@ -151,7 +151,8 @@ public class PinyinTokenizer extends Tokenizer {
                 source = termAtt.toString();
 
                 List<String> pinyinList = Pinyin.pinyin(source);
-                if (pinyinList.size() == 0) return false;
+                List<String> chineseList = ChineseUtil.segmentChinese(source);
+                if (pinyinList.size() == 0 || chineseList.size() == 0) return false;
 
                 StringBuilder buff = new StringBuilder();
                 int buffStartPosition = 0;
@@ -192,6 +193,7 @@ public class PinyinTokenizer extends Tokenizer {
                         }
 
                         String pinyin = pinyinList.get(i);
+                        String chinese = chineseList.get(i);
                         if (pinyin != null && pinyin.length() > 0) {
                             position++;
                             firstLetters.append(pinyin.charAt(0));
@@ -200,6 +202,9 @@ public class PinyinTokenizer extends Tokenizer {
                             }
                             if (config.keepFullPinyin) {
                                 addCandidate(new TermItem(pinyin, i, i + 1, position));
+                            }
+                            if(config.keepSeparateChinese){
+                                addCandidate(new TermItem(chinese, i, i + 1, position));
                             }
                             if (config.keepJoinedFullPinyin) {
                                 fullPinyinLetters.append(pinyin);
